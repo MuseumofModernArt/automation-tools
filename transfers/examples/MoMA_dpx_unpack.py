96a2c4c172a3824620ce6e1230a82ba5879e779b
Ben Fino-Radin <bfinoradin@gmail.com>
2016-02-16 14:57:56 -0500

    more pseudo

--- transfers/examples/MoMA_dpx_unpack.py
+++ transfers/examples/MoMA_dpx_unpack.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python2
 
 '''
-- validate disk image
-- run fiwalk to get DFXML
-- parse DPX CSV to find dir names of components
-- search DFXML for these folders
-– 
+- validate disk image, 
+	- parse disk image log file and searching for line that starts with "Md5 checksum"
+	- run md5 against disk image and make sure it's the same
 
-- parse DFXML and  
-- subprocess: tsk_recover -v -a [disk image] [destination]
 - read DPX metadata file
-- poll TMS API and make component dirs
+- for each line
+	- poll TMS object API endpoint, and get the correlating component include
+	- make directory named with component number --- component id --- object id (get from file name)
+
+- run fiwalk on disk image to get DFXML
+- parse DPX CSV to find dir names of components
+- search DFXML for files that include these folder names in their paths
+– grab the MD5
+- write "data/"+[ filename path from DFXML ], a tab space, and then the MD5 (one line per file)
+
+- subprocess: tsk_recover -v -a [disk image] [destination]
+
 - move DPX folders to their appropriate component dir
 - continue ?
 
